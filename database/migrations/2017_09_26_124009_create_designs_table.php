c7643d1c856cc6928334a432ed735143da57785e
Unknown <glrd.remi@gmail.com>
2017-09-26 14:47:51 +0200

    Create Migration : Design Table

---
+++ database/migrations/2017_09_26_124009_create_designs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDesignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('content')->nullable();
+            $table->integer('level_design');
+            $table->integer('state')->default(3);
+            $table->integer('id_user')->nullable()->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('designs');
+    }
+}
