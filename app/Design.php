04be5c9d082890cdd17c53af1d8c82c355844467
Jean <jean.saunie@devinci.fr>
2017-09-26 16:30:32 +0200

    Create Route/Model/Controller : Designs

---
+++ app/Design.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Design extends Model
+{
+    protected $fillable = ['title', 'content', 'level_design'];
+}
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,15 @@
 |
 */
 
+
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/designs', 'DesignsController@index');
+
 
 Auth::routes();
 
