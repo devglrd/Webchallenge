f7ae7e5d6716574e05d10d6267ba45464e5f693d
Unknown <glrd.remi@gmail.com>
2017-09-26 14:54:59 +0200

    Create Migration : Design_has_integrator table

---
+++ database/migrations/2017_09_26_125019_create_table_design_has_integrator.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableDesignHasIntegrator extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('design_has_integrator', function (Blueprint $table) {
+            $table->integer('id_design');
+            $table->integer('id_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('design_has_integrator');
+    }
+}
