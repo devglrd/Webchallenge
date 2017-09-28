14562a7c9f0d9dd27ae1693957d7295ecd0384f4
Unknown <glrd.remi@gmail.com>
2017-09-28 11:06:52 +0200

    Modify PostTableSeeder

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -21,7 +21,7 @@ class PostsTableSeeder extends Seeder
         for( $i = 0 ; $i < 10 ;$i++){
 
             $posts[] =[
-                'title' => $faker->title(),
+                'title' => $faker->name(rand(1, 12)),
                 'content' => $faker->text($maxNbChars = 200),
                 'id_user' => rand(0, 10),
             ];
